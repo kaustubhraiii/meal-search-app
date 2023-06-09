905c774b98e9505ccc19a77ed63fd7491e7c3a7a
kaustubhraiii <kaustxbhrxi@gmail.com>
2023-06-09 11:51:26 -0700

    added major JS changes and minor CSS changes

--- script.js
+++ script.js
@@ -3,15 +3,47 @@ const mealList = document.getElementById('meal');
 const mealDetailsContent = document.querySelector('.meal-details-content');
 const recipeCloseBtn = document.getElementById('recipe-close-btn');
 
-//event listeners
+// Event listeners
 searchBtn.addEventListener('click', getMealList);
+mealList.addEventListener('click', getMealRecipe);
 
 function getMealList() {
     let searchInputTxt = document.getElementById('search-input').value.trim();
     console.log(searchInputTxt);
-    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast`)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-    });
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
+        .then(response => response.json())
+        .then(data => {
+            let html = "";
+            if (data.meals) {
+                data.meals.forEach(meal => {
+                    html += `
+                    <div class="meal-item" data-id="${meal.idMeal}">
+                        <div class="meal-img">
+                            <img src="${meal.strMealThumb}" alt="food">
+                        </div>
+                        <div class="meal-name">
+                            <h3>${meal.strMeal}</h3>
+                            <a href="#" class="recipe-btn">Get Recipe</a>
+                        </div>
+                    </div>`;
+                });
+                mealList.classList.remove('notFound');
+            } else {
+                html = "Sorry, we didn't find a meal for your ingredient";
+                mealList.classList.add('notFound');
+            }
+            mealList.innerHTML = html;
+        });
+}
+
+
+
+//get recipe of the meal
+function getMealRecipe(e){
+    e.preventDefault();
+    if(e.target.classList.contains('recipe-btn')){
+         let mealItem = e.target.parentElement.parentElement;
+         console.log(mealItem);
+    }
+
 }
